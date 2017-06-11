9e6b84f1ee11b983e0be19538dca7f485f4e9837
Georgi Angelov <georgi.n.angelov@gmail.com>
2017-06-11 16:01:00 +0300

    Solution links fix
    Fixed broken solution links. Solution explorer should now correctly show the projects in solution .

---
+++ C# advanced/04. Numeral Systems/08. Binary Short/BinShort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _08.Short_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short value = short.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(value, 2).PadLeft(16, '0'));
+        }
+    }
+}
